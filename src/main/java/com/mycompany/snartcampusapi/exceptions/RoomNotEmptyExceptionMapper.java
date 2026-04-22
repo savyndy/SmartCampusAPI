4d059cf0354b19d1bfb88e275c3cb30b045fbedd
savyndy <savyndy@gmail.com>
2026-04-23 05:15:34 +0530

    Create RoomNotEmptyExceptionMapper.java

---
+++ src/main/java/com/mycompany/snartcampusapi/exceptions/RoomNotEmptyExceptionMapper.java
@@ -0,0 +1,15 @@
+package com.mycompany.snartcampusapi.exceptions;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class RoomNotEmptyExceptionMapper implements ExceptionMapper<RoomNotEmptyException> {
+    @Override
+    public Response toResponse(RoomNotEmptyException ex) {
+        return Response.status(Response.Status.CONFLICT)
+                .entity("{\"error\":\"" + ex.getMessage() + "\"}")
+                .type("application/json")
+                .build();
+    }
+}

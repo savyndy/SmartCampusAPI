42b6252787ccedcc5529d0f7c5168d8acc64c63c
savyndy <savyndy@gmail.com>
2026-04-23 05:15:21 +0530

    Create LinkedResourceNotFoundException.java

---
+++ src/main/java/com/mycompany/snartcampusapi/exceptions/LinkedResourceNotFoundException.java
@@ -0,0 +1,4 @@
+package com.mycompany.snartcampusapi.exceptions;
+public class LinkedResourceNotFoundException extends RuntimeException {
+    public LinkedResourceNotFoundException(String message) { super(message); }
+}

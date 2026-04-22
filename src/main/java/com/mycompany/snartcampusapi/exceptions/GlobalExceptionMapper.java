4e123b4d3820c829c5885b96457e1755bbabcb9e
savyndy <savyndy@gmail.com>
2026-04-23 05:15:18 +0530

    Create GlobalExceptionMapper.java

---
+++ src/main/java/com/mycompany/snartcampusapi/exceptions/GlobalExceptionMapper.java
@@ -0,0 +1,15 @@
+package com.mycompany.snartcampusapi.exceptions;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class GlobalExceptionMapper implements ExceptionMapper<Throwable> {
+    @Override
+    public Response toResponse(Throwable ex) {
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                .entity("{\"error\":\"An unexpected internal server error occurred.\"}")
+                .type("application/json")
+                .build();
+    }
+}

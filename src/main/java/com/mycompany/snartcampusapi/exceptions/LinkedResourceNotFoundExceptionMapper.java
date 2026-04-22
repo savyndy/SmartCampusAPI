33e8c17e6bee37ac42cd974bcd4568c400eccb15
savyndy <savyndy@gmail.com>
2026-04-23 05:15:24 +0530

    Create LinkedResourceNotFoundExceptionMapper.java

---
+++ src/main/java/com/mycompany/snartcampusapi/exceptions/LinkedResourceNotFoundExceptionMapper.java
@@ -0,0 +1,15 @@
+package com.mycompany.snartcampusapi.exceptions;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class LinkedResourceNotFoundExceptionMapper implements ExceptionMapper<LinkedResourceNotFoundException> {
+    @Override
+    public Response toResponse(LinkedResourceNotFoundException ex) {
+        return Response.status(422) // 422 Unprocessable Entity
+                .entity("{\"error\":\"" + ex.getMessage() + "\"}")
+                .type("application/json")
+                .build();
+    }
+}
